2b6e84d80686b42541c53cd5b5dbae6d37b705e6
Isaac Easton <iae1@protonmail.com>
2021-11-08 14:37:47 -0500

    Builds react side

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import * as React from "react"
+import { render } from "react-dom"
+import { createStore, applyMiddleware, Store } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App"
+import reducer from "./store/reducer"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store: Store<ArticleState, ArticleAction> & { dispatch: DispatchType } = createStore(reducer, applyMiddleware(thunk))
+
+ const rootElement = document.getElementById("root")
+
+ render(
+ <Provider store={store}>
+   <App />
+ </Provider>,
+ rootElement
+ )
